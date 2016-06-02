1a8e7ddaf5de38c6ea31d0465e4fd1460054553a
Mattias Evensson <mattias@evensson.eu>
2016-06-02 08:03:05 +0200

    Create gradle project

---
+++ src/main/java/eu/evensson/primenumbers/Main.java
@@ -0,0 +1,7 @@
+package eu.evensson.primenumbers;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/eu/evensson/primenumbers/MyTest.java
@@ -0,0 +1,13 @@
+package eu.evensson.primenumbers;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class MyTest {
+	@Test
+	public void shouldSucceed() throws Exception {
+		assertThat(true, is(true));
+	}
+}
